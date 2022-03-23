f6ff88d080467eace2814a30611fb71f31764b4c
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2022-03-23 16:53:17 -0500

    Got State machine started

--- raygame/MainScene.cpp
+++ raygame/MainScene.cpp
@@ -10,23 +10,5 @@
 
 void MainScene::start()
 {
-	Player* player = new Player(200, 50, "Player", 100, 50);
-	player->getTransform()->setScale({ 50,50 });
-	player->addComponent(new SpriteComponent("Images/player.png"));
-
-	Agent* agent = new Agent(0, 0, "Agent", 200, 500);
-	agent->getTransform()->setScale({ 50,50 });
-	agent->addComponent(new SpriteComponent("Images/enemy.png"));
-
-	WanderComponent* wanderComponent = new WanderComponent(1000, 100, 100);
-	agent->addComponent(wanderComponent);
-
-	SeekComponent* seekComponent = new SeekComponent();
-	seekComponent->setSteeringForce(50);
-	seekComponent->setTarget(player);
-	agent->addComponent(seekComponent);
-	agent->addComponent<StateMachineComponent>();
-
-	addActor(player);
-	addActor(agent);
+	
 }
--- raygame/StateMachineComponent.h
+++ raygame/StateMachineComponent.h
@@ -5,29 +5,28 @@ enum State
 {
 	IDLE,
 	WANDER,
-	SEEK
+	Track
 };
 
-class SeekComponent;
 class WanderComponent;
+class PathfindComponent;
 
-class StateMachineComponent :
+class StateMachineComponent : 
 	public Component
 {
 public:
 	void start() override;
 
 	State getCurrentState() { return m_currentState; }
-	void setCurrentState(State state) { m_currentState = state; }
+	void setCurrentState(State state) { m_currentState = state; }	
 
 	void update(float deltaTime) override;
 
 private:
 	State m_currentState;
-	SeekComponent* m_seekComponent;
 	WanderComponent* m_wanderComponent;
+	PathfindComponent* m_pathfindComponent;
 	float m_seekForce;
-	float m_wanderForce;
-	float m_seekRange = 300;
+	float m_seekRange = 150;
 };
 
